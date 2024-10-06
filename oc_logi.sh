28895419bbc7f27041b66d9764745718a066c1f8
Berhane <Berhanewells@gmail.com>
2024-10-06 08:34:52 -0400

    Change a php application

---
+++ oc_logi.sh
@@ -0,0 +1,1 @@
+oc login --token=sha256~JYWuoiUgqXpqxIcKXXqHMh_3Ur8T349wtk7amI1_MQM --server=https://api.sandbox-m4.g2pi.p1.openshiftapps.com:6443
--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,4 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "A Change is coming - a Great change at that God is Good!\n"
 ?>
